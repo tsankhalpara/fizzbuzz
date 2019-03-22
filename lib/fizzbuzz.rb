ada6059583a8a71c60bc68265dcd6afa8fa467d5
Tara Sankhalpara <tsankhalpara@mac.com>
2019-03-22 17:14:17 +0000

    add def to fizzbuzz file

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,13 @@
-1.upto(20) do |n|
-  if n % 3 == 0 and n % 5 == 0
-    puts "FizzBuzz"
-  elsif n % 3 == 0
-    puts "Fizz"
-  elsif n % 5 == 0
-    puts "Buzz"
-  else
-    puts n
+def fizzbuzz
+  1.upto(20) do |n|
+    if n % 3 == 0 and n % 5 == 0
+      puts "FizzBuzz"
+    elsif n % 3 == 0
+      puts "Fizz"
+    elsif n % 5 == 0
+      puts "Buzz"
+    else
+      puts n
+    end
   end
 end
