60d03d963ef71e3b35522950c2a39fd72b01b89e
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-03-22 17:26:15 +0000

    add method for fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect (fizzbuzz(5)) == 'buzz'
   end
+  it 'returns "fizzbuzz" when passed 5' do
+    expect (fizzbuzz(15)) == 'fizzbuzz'
+  end
 end
