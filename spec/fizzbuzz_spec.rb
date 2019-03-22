019c7efdac0a48d06600de591be223c6d75f7ac4
Tara Sankhalpara <tsankhalpara@mac.com>
2019-03-22 17:24:07 +0000

    add test for 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect (fizzbuzz(3)) == 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect (fizzbuzz(5)) == 'buzz'
+  end
 end
